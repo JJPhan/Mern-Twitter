cbb5be10fde3c88367a8acd346f6e7b4037691ea
Jordan Phan <JordanJPhan@gmail.com>
2020-08-30 21:12:21 -0700

    update key

--- config/keys_dev2.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mongoURI:
-        // "mongodb://<DBUser>:<password>@cluster.mongodb.net/test?retryWrites=true&w=majority",
-        "mongodb+srv://dev:PxNj8XByKpOynI0k@cluster0.gvfq4.mongodb.net/MERNdb?retryWrites=true&w=majority",
-    secretOrKey:
-        "secret",
-    //Make sure this is your own unique string
-};
